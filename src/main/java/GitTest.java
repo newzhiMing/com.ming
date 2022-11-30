ce9f50001c3d6b9754b37cc13fb52858e2111f6c
ming <ming@.com>
2022-11-30 22:55:41 +0800

    idea first Commit

---
+++ src/main/java/GitTest.java
@@ -0,0 +1,10 @@
+/**
+ * @author: Mr.ming
+ * @describe: NULL
+ * @create: 2022/11/30-22:54
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("Hello,Git");
+    }
+}
