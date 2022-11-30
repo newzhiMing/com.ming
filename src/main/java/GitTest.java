28dc02f68d90b9eb2500b98a14d831c7da2ac1b2
ming <ming@.com>
2022-12-01 00:13:23 +0800

    push Commit2

--- src/main/java/GitTest.java
+++ src/main/java/GitTest.java
@@ -12,5 +12,6 @@ public class GitTest {
         System.out.println("master test!");
         System.out.println("Hot-fix test!");
          System.out.println("PULL test!");
+         System.out.println("Push test!");
     }
 }
